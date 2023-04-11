12d94cf8eaf8a5c297367edb88e9bd9a4056cd81
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-04-11 15:48:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JURNAL8_1302210078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("haii");
+        }
+    }
+}
